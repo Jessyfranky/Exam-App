9ecaade5529c306ca706e7b32b68377e5cc1007a
Jesse Tech <115389742+Jessyfranky@users.noreply.github.com>
2025-03-17 11:59:53 +0100

    Update App.js

--- exam-app-frontend/src/App.js
+++ exam-app-frontend/src/App.js
@@ -1,27 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage.js";           // Normal user login
-import RegisterPage from "./pages/RegisterPage.js";       // Normal user registration
+import HomePage from "./pages/HomePage.js";
+import LoginPage from "./pages/LoginPage.js";
+import RegisterPage from "./pages/RegisterPage.js";
 import Dashboard from "./pages/Dashboard.js";
 import ExamPage from "./pages/ExamPage.js";
-import AdminLoginPage from "./pages/AdminLoginPage.js";   // Admin login
-import AdminRegisterPage from "./pages/AdminRegisterPage.js"; // Admin registration
-import AdminPanel from "./pages/AdminPanel.js";           // Admin panel
+import AdminPanel from "./pages/AdminPanel.js";
+import AdminLoginPage from "./pages/AdminLoginPage.js";
+import AdminRegisterPage from "./pages/AdminRegisterPage.js";
 import ProtectedRoute from "./components/ProtectedRoute.js";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        
         {/* User Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route
           path="/dashboard"
-          element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
         />
         <Route
           path="/exam/:examId"
-          element={<ProtectedRoute><ExamPage /></ProtectedRoute>}
+          element={
+            <ProtectedRoute>
+              <ExamPage />
+            </ProtectedRoute>
+          }
         />
 
         {/* Admin Routes */}
@@ -29,7 +40,11 @@ function App() {
         <Route path="/admin/register" element={<AdminRegisterPage />} />
         <Route
           path="/admin"
-          element={<ProtectedRoute><AdminPanel /></ProtectedRoute>}
+          element={
+            <ProtectedRoute>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
         />
       </Routes>
     </Router>
