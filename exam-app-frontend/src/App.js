08816249c3ba08cac1bbc484a5e31264bdd7b462
Jesse Tech <115389742+Jessyfranky@users.noreply.github.com>
2025-03-18 09:17:17 +0100

    Update App.js

--- exam-app-frontend/src/App.js
+++ exam-app-frontend/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage.js";
 import RegisterPage from "./pages/RegisterPage.js";
 import Dashboard from "./pages/Dashboard.js";
 import ExamPage from "./pages/ExamPage.js";
+import VerifyEmailPage from "./pages/VerifyEmailPage.js"; // New verification page
 import AdminPanel from "./pages/AdminPanel.js";
 import AdminLoginPage from "./pages/AdminLoginPage.js";
 import AdminRegisterPage from "./pages/AdminRegisterPage.js";
@@ -18,34 +19,14 @@ function App() {
         {/* User Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/exam/:examId"
-          element={
-            <ProtectedRoute>
-              <ExamPage />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/exam/:examId" element={<ProtectedRoute><ExamPage /></ProtectedRoute>} />
+        
         {/* Admin Routes */}
         <Route path="/admin/login" element={<AdminLoginPage />} />
         <Route path="/admin/register" element={<AdminRegisterPage />} />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminPanel />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/admin" element={<ProtectedRoute><AdminPanel /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
